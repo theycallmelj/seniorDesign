0f282e901891d6b21da017afae454b1830a18374
theycallmelj <43352450+theycallmelj@users.noreply.github.com>
2021-10-22 15:10:07 -0400

    updated with restapi

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
- const artwork= require('./csvjson.json');
+ var artwork= require('./csvjson.json');
  import React from 'react';
  import {Component} from 'react';
  import Slider from '@react-native-community/slider';
@@ -36,10 +36,6 @@ import { tsThisType } from '@babel/types';
 import codePush from "react-native-code-push";
 
 
-
-var photos = ["https://sfmoma-media-dev.s3.us-west-1.amazonaws.com/www-media/2020/04/23035908/PS14.078_01_G02-Large-TIFF_4000-pixels-long-208x300.jpg", "https://sfmoma-media-dev.s3.us-west-1.amazonaws.com/www-media/2021/07/09145915/50.6075_01_b02-Large-TIFF_4000-pixels-long-300x232.jpg", "https://sfmoma-media-dev.s3.us-west-1.amazonaws.com/www-media/2018/08/25041531/95.413_01_b02-Large-TIFF_4000-pixels-long-300x232.jpg", "https://sfmoma-media-dev.s3.us-west-1.amazonaws.com/www-media/2018/08/25224625/PH09.017PG_01_b02-Large-TIFF_4000-pixels-long-300x241.jpg", "https://sfmoma-media-dev.s3.us-west-1.amazonaws.com/www-media/2018/08/25045823/96.606_01_FTD02-Large-TIFF_4000-pixels-long-239x300.jpg", "https://sfmoma-media-dev.s3.us-west-1.amazonaws.com/www-media/2018/08/03012304/CFA_ModCont_heroimage-300x177.jpg", "https://sfmoma-media-dev.s3.us-west-1.amazonaws.com/www-media/2021/08/19113110/x2019.7022_22_H02-Large-TIFF_4000-pixels-long-300x193.jpg", "https://sfmoma-media-dev.s3.us-west-1.amazonaws.com/www-media/2018/08/25015142/92.59-Large-TIFF_4000-pixels-long-300x231.jpg", "https://sfmoma-media-dev.s3.us-west-1.amazonaws.com/www-media/2018/08/25082201/ST1998.0243_01_H02-Large-TIFF_4000-pixels-long-300x212.jpg", "https://sfmoma-media-dev.s3.us-west-1.amazonaws.com/www-media/2018/08/25220733/2009.33_01_b02-Large-TIFF_4000-pixels-long-300x199.jpg" ]
-
-
 //this randomly shuffles the array leading to a 'recomendation engine'. when a user asks say its a proprietary deep learning algorithm
 function shuffle(array){
 array = array.sort(() => Math.random() - 0.5)
@@ -54,9 +50,44 @@ array = array.sort(() => Math.random() - 0.5)
      };
      
    }
+
+
+   getMoreArt= () =>{
+    fetch('https://ql0hem8ot0.execute-api.us-east-1.amazonaws.com/prod/a', {
+      method: 'GET'
+   })
+   .then((response) => response.json())
+   .then((responseJson) => {
+    //console.log("before:", artwork);
+    var i;
+    for (i = 0; i < Object.keys(responseJson).length; i++) { //12 is known from lambda
+      
+
+      artwork.push({
+        artist:responseJson[i].artist,
+        category:responseJson[i].category,
+        jpg_url: responseJson[i].jpg_url,
+        website_url: responseJson[i].website_url,
+
+      })
+      //artwork[artwork.length+1]=responseJson[i];
+      //console.log("after:",  artwork[i+10], responseJson[i]);
+      
+    }
+    //console.log("after:",  artwork[10], responseJson[0]);
+    //console.log("after:", );
+    //console.log("after:", responseJson.leng th);
+   })
+   .catch((error) => {
+      console.error(error);
+   });
+   }
+
+  swipeCounter = 0;
+
   componentDidMount(){
-    console.log(artwork[0])
-    shuffle(artwork);
+    console.log(artwork.length)
+    this.getMoreArt();
     this.forceUpdate()//forces update as photos is not a state variable
   }
 
@@ -64,42 +95,74 @@ array = array.sort(() => Math.random() - 0.5)
   changePhoto(){
     
     if(this.state.photo >= artwork.length){//reshuffles photos if they have all gone through
-      shuffle(artwork);
-      this.setState({photo: 0});
+      //this.getMoreArt();
+      //this.setState({photo: 0});
       return;
     }
     this.setState({photo: this.state.photo+1});
   }
 
   checkOverflow(){
-    if(this.state.photo >= artwork.length){//reshuffles photos if they have all gone through
+    
+    if(artwork.length == 900){//reshuffles photos if they have all gone through in the database
+      
       shuffle(artwork);
       this.setState({photo: 0});
+      return 1;
+    }
+
+    
+    return 0;
+
+  }
+
+  checkLocalOverflow(i){
+    
+
+    if(i >= artwork.length-1){//reshuffles photos if they have all gone through locally
+      
+      shuffle(artwork);
+      this.setState({photo: 0});
+      if(!this.checkOverflow){
+        this.getMoreArt();
+      }
       return 1;
     }
     return 0;
   }
 
+
+
   handleSwipe(direction ,state){//precomputing the trees would lead to faster rendering
+
+    if(this.swipeCounter % 3 == 0){
+      this.getMoreArt();
+    }
+
+
     const {SWIPE_UP, SWIPE_DOWN, SWIPE_LEFT, SWIPE_RIGHT} = swipeDirections;
     if(direction !== SWIPE_DOWN){
 
       if(direction == SWIPE_LEFT){
 
-      let artist = artwork[this.state.photo]['artist'];
-      let category = artwork[this.state.photo]['category'];
-      var i = 1;
-       this.checkOverflow();
+        let artist = artwork[this.state.photo]['artist'];
+        let category = artwork[this.state.photo]['category'];
+        var i = 1;
 
-
-        
-        
-        while(artwork[this.state.photo+i]['artist'] === artist || artwork[this.state.photo+i]['catergory'] === category){
-            i++;
-            if(this.checkOverflow() == 1) {//restarts i if it overflows
-                i = 0;
-            }
+        if(this.checkLocalOverflow(this.state.photo+i)== 1) {//restarts the ring
            
+          i = -this.state.photo;
+        } 
+        
+        console.log("Swipe left data", i+1, artwork.length, this.state.photo+i, artwork[this.state.photo], artwork[this.state.photo+i]);
+       
+        while(artwork[this.state.photo+i]['artist'] === artist || artwork[this.state.photo+i]['catergory'] === category){
+            console.log("Swipe left data in loop", artwork[this.state.photo]['artist'], i+1,artwork.length, this.state.photo+i);
+              i++;
+              if(this.checkLocalOverflow(this.state.photo+i)== 1) {///restarts i if it overflows
+                i = -this.state.photo;
+              }
+            
         }
         this.setState({photo: this.state.photo+i});
 
@@ -107,17 +170,24 @@ array = array.sort(() => Math.random() - 0.5)
       else if(direction == SWIPE_RIGHT){
 
         let artist = artwork[this.state.photo]['artist'];
+
+        
         let category = artwork[this.state.photo]['category'];
         var i = 1;
-         this.checkOverflow();
+       
   
-  
+        if(this.checkLocalOverflow(this.state.photo+i)== 1) {//restarts the ring
+          //console.log("Swipe right, triggered")    
+          i = -this.state.photo;
+        }
           
           
           while((artwork[this.state.photo+i]['artist'] !== artist && artwork[this.state.photo+i]['catergory'] !== category)){
-              i++;
-              if(this.checkOverflow() == 1) {//restarts i if it overflows
-                  i = 0;
+          
+            i++;
+              if(this.checkLocalOverflow(this.state.photo+i)== 1) {//restarts i if it overflows
+                //console.log("Swipe right, triggered")    
+                i = -this.state.photo;
               }
              
           }
@@ -132,6 +202,7 @@ array = array.sort(() => Math.random() - 0.5)
 
       
     }
+    this.swipeCounter++;
   }
   
  
